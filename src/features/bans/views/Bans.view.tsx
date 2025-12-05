ab525bd83b1f7a5f41a9bef6be886933e69fb82a
jhosuapp <jhosuapvll@gmail.com>
2025-12-05 10:18:44 -0500

    fix: ui

--- src/features/bans/views/Bans.view.tsx
+++ src/features/bans/views/Bans.view.tsx
@@ -3,7 +3,7 @@ import { CardBan  } from "../components"
 
 const BansView = ():JSX.Element => {
     return (
-        <Container className="!mt-5 lg:!mt-10" isFirst>
+        <Container className="!mt-5 lg:!mt-10" isFirst isLast>
             <CardWrappersecondary title={ `Castigos activos (${4})` } text={`${3} Usuarios`}>
                 <CardBan 
                     username="danjoh_0409"
