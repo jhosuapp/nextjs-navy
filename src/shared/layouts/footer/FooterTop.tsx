6f35e55a48637118567d8319f3ac82dd1c7156a3
jhosuapp <jhosuapvll@gmail.com>
2025-12-05 10:27:09 -0500

    feat: discord

--- src/shared/layouts/footer/FooterTop.tsx
+++ src/shared/layouts/footer/FooterTop.tsx
@@ -6,8 +6,8 @@ const FooterTop = ( ):JSX.Element => {
         <section className={ styles.footerTop }>
             <ul>
                 <li>©2024 navy - <a className='hoverLine' href="https://github.com/jhosuapp" target='_blank'>jhosuapp</a></li>
-                <li>Discord <a className='hoverLine' href="https://discord.gg/navyy" target='_blank'>discord.gg/navyy</a></li>
-                <li>Desarrollado por <a className='hoverLine' href="https://github.com/jhosuapp" target='_blank'> jhosuapp </a> 💜</li>
+                <li>Discord - <a className='hoverLine' href="https://discord.gg/navyy" target='_blank'>discord.gg.navyy</a></li>
+                <li>Desarrollado por - <a className='hoverLine' href="https://github.com/jhosuapp" target='_blank'> jhosuapp </a> 💜</li>
             </ul>
         </section>
     )
